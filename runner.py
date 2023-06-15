8df369b8ef4190739438b475a0e105a4d6c891d9
Hamish McKinnon <114838576+hlmcki@users.noreply.github.com>
2023-06-15 21:14:19 +1000

    Added files via upload

---
+++ runner.py
@@ -0,0 +1,20 @@
+from scrabble import *
+
+# Prompt user for number of players
+players = int(input("Number of players: "))
+
+# Create game and AI agent
+game = Scrabble(players=players)
+ai = ScrabbleAI()
+
+count = 0
+
+while count < 3:
+    game.print()
+    print("Turn:      Player", game.turn)
+    print("Hand:     ", game.hands[game.turn])
+    word = input("Word:      ")
+    start = (input("Start:     "))
+    direction = input("Direction: ")
+    game.play_word(word, start, direction)
+    count += 1
